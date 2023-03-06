03b2aeff1ee581e1b86f5c95029f09698d7853f1
eddy <ggfxck@naver.com>
2023-03-06 23:52:46 +0900

    인터셉터 로그 삭제

--- src/main/java/com/oph/controller/OphInterceptorController.java
+++ src/main/java/com/oph/controller/OphInterceptorController.java
@@ -40,18 +40,11 @@ public class OphInterceptorController  implements HandlerInterceptor{
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             ModelAndView modelAndView) throws Exception {
-        
-        System.out.println("postHandle1");
-        
-        
     }
  
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
             throws Exception {
-        
-        System.out.println("afterCompletion1");
-        
         
     }    
     
